f83a0ff4855ae8be2e0282bb3bfb160ed1b36af8
Kauana Henriques <karoltvbl@gmail.com>
2022-10-04 23:24:51 -0300

    implenta metodo para entrar no menu

--- app/src/main/java/com/example/emporio_organico/view/MainActivity.java
+++ app/src/main/java/com/example/emporio_organico/view/MainActivity.java
@@ -1,8 +1,12 @@
-package com.example.emporio_organico;
+package com.example.emporio_organico.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.emporio_organico.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void entrarMenu(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
