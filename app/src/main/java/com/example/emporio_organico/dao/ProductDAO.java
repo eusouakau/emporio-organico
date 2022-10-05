fe5a31640f75e2bb0c5bf1bd89c0d888dd0c5ca3
Kauana Henriques <karoltvbl@gmail.com>
2022-10-04 23:26:16 -0300

    implementa interface dao de produto

---
+++ app/src/main/java/com/example/emporio_organico/dao/ProductDAO.java
@@ -0,0 +1,33 @@
+package com.example.emporio_organico.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import com.example.emporio_organico.entity.Product;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+@Dao
+public interface ProductDAO {
+
+    @Query("SELECT * FROM Product")
+    public List<Product> getAllProduct();
+
+    @Query("SELECT * FROM Product WHERE nome = :name")
+    public List<Product> geProductByName(String name);
+
+    @Insert(onConflict = REPLACE)
+    public void insert(Product product);
+
+    @Update
+    public void update(Product product);
+
+    @Delete
+    public void delete(Product product);
+
+}
